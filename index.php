9535d8c1b5bcef1fde2fb441c024bac142f8883c
champiiy <albert1047@hotmail.com>
2018-05-31 11:47:40 -0500

    ADD RESPONSIVO E SENDCORREO

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    
+
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <title>CONTROL Y PROYECTOS CREATIVOS SA DE CV</title>
-   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <META NAME="Title" content="CONTROL Y PROYECTOS CREATIVOS SA DE CV">
     <META NAME="Author" CONTENT="[http://controlyproyectoscreativos.com/]"/>
     <META name="Description" content="Somos una compañía dedicados a ofrecer construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones."/>
     <META NAME="Keywords" CONTENT="construcción , construcción COYP , COYP , contrucción de casas , ampliaciones de casa , ingenieria civil , productos de construcción, levantamientos, desarrollo de proyectos de construcción"/>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/estilos.css">
-    <!-- <link rel="stylesheet" href="css/responsivo.css"> -->
+    <link rel="stylesheet" href="css/responsivo.css">
     <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
-    
+    <script src="js/general.js"></script>
     <script src="js/fontawesome-all.js"></script>
+
 
 
 
@@ -45,18 +46,21 @@
               <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
             </ul>
           </nav>
-        
+
           <figure class="logo pull-right">
             <img src="img/control.png" alt="CONTROL Y PROYECTOS CREATIVOS" class="img-responsive">
           </figure>
-         
+
+          <div class="openclose">
+            <i class="fa fa-bars open" aria-hidden="true"></i>
+          </div>
         </div>
 
     <div class="banner"  id="INICIO">
       <div class="fondo">
         <div class="tit">Despacho de Arquitectos , especialistas en construcción</div>
         <center style="margin-top: 3%;">
-          <a data-scroll href="#CONTACTO" aria-pressed="true"><button class="btn btn-lg btn-secondary">CONTACTANOS</button></a> 
+          <a data-scroll href="#CONTACTO" aria-pressed="true"><button class="btn btn-lg btn-secondary">CONTACTANOS</button></a>
         </center>
       </div>
     </div>
@@ -70,24 +74,25 @@
 
   <div class="section">
       <div class="container">
-        <div class="row">
-          <div class="col-md-2">
+        <div class="row cont-pe">
+          <div class="col-sm-2 col-md-2">
             <div class="iconos">
               <i class="far fa-address-book fa-7x"></i>
             </div>
           </div>
-          <div class="col-md-4">
-            <h3 class="text-left">Planeación de Proyectos</h3>
-            <p class="text-left">Nuestra misión es llevar tu proyecto de tu visión a una realidad. Nuestro equipo de arquitectos y diseñadoras trabajarán contigo en aterrizar el concepto y las necesidades que tengas para asegurar tener un proyecto exitoso. Nos aseguramos de tener tu proyecto 100% basado en tus necesidades acompañado de recomendaciones de mejora para lograr la satisfacción total en tu proyecto.</p>
+          <div class="col-sm-4 col-md-4">
+            <h3 class="text-left tit-pe">Planeación de Proyectos</h3>
+            <p class="text-left txt-pe">Nuestra misión es llevar tu proyecto de tu visión a una realidad. Nuestro equipo de arquitectos y diseñadoras trabajarán contigo en aterrizar el concepto y las necesidades que tengas para asegurar tener un proyecto exitoso. Nos aseguramos de tener tu proyecto 100% basado en tus necesidades acompañado de recomendaciones de mejora para lograr la satisfacción total en tu proyecto.</p>
           </div>
-          <div class="col-md-2">
+          <br class="delete-br">
+          <div class="col-sm-2 col-md-2">
             <div class="iconos">
               <i class="fas fa-users-cog fa-7x"></i>
             </div>
           </div>
-          <div class="col-md-4">
-            <h3 class="text-left">Ejecución</h3>
-            <p class="text-left">Trabajamos con diversas técnicas de diseño y planeación antes de construir para tener bien capturadas tus ideas. Creamos los diseños en 3D ya que es la forma más fácil de asegurar ir por el camino correcto.</p>
+          <div class="col-sm-4 col-md-4">
+            <h3 class="text-left tit-pe">Ejecución</h3>
+            <p class="text-left txt-pe">Trabajamos con diversas técnicas de diseño y planeación antes de construir para tener bien capturadas tus ideas. Creamos los diseños en 3D ya que es la forma más fácil de asegurar ir por el camino correcto.</p>
           </div>
         </div>
       </div>
@@ -95,75 +100,63 @@
 
     <div class="banner2">
       <div class="fondo2">
-        <p class="text-center texto">¿Tienes una idea para tu proyecto?</p> 
+        <p class="text-center texto">¿Tienes una idea para tu proyecto?</p>
         <p class="text-center texto2">¡Te ayudamos a que sea realidad!</p>
         <center style="margin-top: 3%;">
-          <a data-scroll href="#CONTACTO" aria-pressed="true"><button class="btn btn-lg btn-secondary">¡Solicita informes aqui!</button></a> 
+          <a data-scroll href="#CONTACTO" aria-pressed="true"><button class="btn btn-lg btn-secondary">¡Solicita informes aqui!</button></a>
         </center>
       </div>
     </div>
-  
+
 </section>
 
 <section class="seccion1" id="SERVICIOS" style="padding: 5% 0%; background: #f5eef8;" >
 
   <div class="section">
-    
+
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <h1 class="text-center">Qué hacemos</h1> <br>
-            <p class="text-center" style="font-size: 19px;">Somos expertos en <b>diseño</b> y <b>construcción</b> de proyectos residenciales y preparación de terreno para construcción</p>
+            <h1 class="text-center tit-qh">Qué hacemos</h1> <br>
+            <p class="text-center txt-qh" style="font-size: 19px;">Somos expertos en <b>diseño</b> y <b>construcción</b> de proyectos residenciales y preparación de terreno para construcción</p>
           </div>
-        </div> <br><br> 
+        </div> <br><br>
         <div class="row">
-          <div class="col-md-4">
-            <div class="imagen">
-              
-            </div>
-            <h2 class="text-center">Proyectos residenciales e industriales</h2>
-            <p class="text-center">Creamos proyectos arquitectónicos y ejecutivos. Nuestros entregables consisten en planos, renders, maquetas y recorridos virtuales.</p>
+          <div class="col-sm-4 col-md-4">
+            <div class="imagen"></div>
+            <h2 class="text-center tit-prcnec">Proyectos residenciales e industriales</h2>
+            <p class="text-center txt-prcnec">Creamos proyectos arquitectónicos y ejecutivos. Nuestros entregables consisten en planos, renders, maquetas y recorridos virtuales.</p>
           </div>
-          <div class="col-md-4">
-            <div class="imagen2">
-              
-            </div>
-            <h2 class="text-center">Remodelaciones y acabados</h2>
-            <p class="text-center">Utilizamos materiales de la más alta calidad en todos nuestros proyectos de construcción, con instalaciones profesionales e impecables.</p>
+          <div class="col-sm-4 col-md-4">
+            <div class="imagen2"></div>
+            <h2 class="text-center tit-prcnec">Remodelaciones y acabados</h2>
+            <p class="text-center txt-prcnec">Utilizamos materiales de la más alta calidad en todos nuestros proyectos de construcción, con instalaciones profesionales e impecables.</p>
           </div>
-          <div class="col-md-4">
-            <div class="imagen3">
-              
-            </div>
-            <h2 class="text-center">Construcción de obra civil</h2>
-            <p class="text-center">Ponemos a tu disposición todos los servicios relacionados con la obra civil para tus proyectos de construcción y remodelación.</p>
+          <div class="col-sm-4 col-md-4">
+            <div class="imagen3"></div>
+            <h2 class="text-center tit-prcnec">Construcción de obra civil</h2>
+            <p class="text-center txt-prcnec">Ponemos a tu disposición todos los servicios relacionados con la obra civil para tus proyectos de construcción y remodelación.</p>
           </div>
         </div>
 
-        <br><br> 
+        <br class="delete-br-d"><br class="delete-br-d">
         <div class="row">
-          <div class="col-md-4">
-            <div class="imagen4">
-              
-            </div>
-            <h2 class="text-center">Naves industriales</h2>
-            <p class="text-center">Realizamos proyectos de diseño y construcción de naves industriales y estructuras metálicas.</p>
+          <div class="col-sm-4 col-md-4">
+            <div class="imagen4"></div>
+            <h2 class="text-center tit-prcnec">Naves industriales</h2>
+            <p class="text-center txt-prcnec">Realizamos proyectos de diseño y construcción de naves industriales y estructuras metálicas.</p>
           </div>
-          <div class="col-md-4">
-            <div class="imagen5">
-              
-            </div>
-            <h2 class="text-center">Electricidad en alta y baja tensión</h2>
-            <p class="text-center">Abarcamos proyectos de baja y alta tensión. Hacemos desde la instalación básica de focos hasta la construcción de subestaciones para edificios.</p>
+          <div class="col-sm-4 col-md-4">
+            <div class="imagen5"></div>
+            <h2 class="text-center tit-prcnec">Electricidad en alta y baja tensión</h2>
+            <p class="text-center txt-prcnec">Abarcamos proyectos de baja y alta tensión. Hacemos desde la instalación básica de focos hasta la construcción de subestaciones para edificios.</p>
           </div>
-          <div class="col-md-4">
-            <div class="imagen6">
-              
-            </div>
-            <h2 class="text-center">Climas y ductos</h2>
-            <p class="text-center">Fabricamos la ductería, hacemos la instalación y suministramos los aparatos para climatización central.</p>
+          <div class="col-sm-4 col-md-4">
+            <div class="imagen6"></div>
+            <h2 class="text-center tit-prcnec">Climas y ductos</h2>
+            <p class="text-center txt-prcnec">Fabricamos la ductería, hacemos la instalación y suministramos los aparatos para climatización central.</p>
           </div>
-          
+
         </div>
       </div>
     </div>
@@ -173,36 +166,37 @@
 
 <section class="seccion3" id="CONTACTO" style="padding: 5% 0% 5% 0%;">
   <div class="container">
-    <h1>Cotiza sin compromiso</h1>
+    <h1 class="tit-cc">Cotiza sin compromiso</h1>
   <div class="row">
-    <form role="form" id="contact-form" class="contact-form">
+    <form role="form" id="contact-form" class="contact-form" onSubmit="return false">
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-sm-4 col-md-4">
           <div class="form-group">
-            <input type="text" class="form-control" name="Name" autocomplete="off" id="Name" placeholder="Escribe tu nombre completo">
+            <input type="text" class="form-control" name="Name" autocomplete="off" id="txtNOMBRE" placeholder="Escribe tu nombre completo">
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-4 col-md-4">
           <div class="form-group">
-            <input type="email" class="form-control" name="email" autocomplete="off" id="email" placeholder="Escribe tu correo">
+            <input type="email" class="form-control" name="email" autocomplete="off" id="txtCORREO" placeholder="Escribe tu correo">
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-4 col-md-4">
           <div class="form-group">
-            <input type="text" class="form-control" name="Tel" autocomplete="off" id="Tel" placeholder="Escribe tu número teléfonico">
+            <input type="number" class="form-control" name="Tel" autocomplete="off" id="txtTELEFONO" placeholder="Escribe tu número teléfonico">
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
           <div class="form-group">
-            <textarea class="form-control textarea" rows="3" name="Message" id="Message" placeholder="Escribe tu comentario"></textarea>
+            <textarea class="form-control textarea" rows="3" name="Message" id="txtCOMENTARIO" placeholder="Escribe tu comentario"></textarea>
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
-          <button type="submit" class="btn main-btn pull-right">Enviar mensaje</button>
+          <div id="_AJAX_PRE_"></div>
+          <button type="submit" class="btn main-btn pull-right" onclick="sendCORREO()">Enviar mensaje</button>
         </div>
       </div>
     </form>
@@ -217,11 +211,12 @@
 </footer>
 
 
+    <script src="js/jquery.js"></script>
     <script src="js/sendCORREO.js"></script>
+    <script src="js/menu.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.js"></script>
     <script src="js/arriba.js"></script>
     <script src="js/bootstrap.js"></script>
   </body>
