5fae2b5db0481e71a4172b654935a96a74fe655e
esteban1234 <tva_libra@hotmail.com>
2018-06-05 13:45:11 -0500

    metas

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-
+    <meta name="google-site-verification" content="lBRnA6dJwNnx_r8GfrFmOSxE_QiyWyOeqoC0bkOE94Y" />
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <title>CONTROL Y PROYECTOS CREATIVOS SA DE CV</title>
    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
